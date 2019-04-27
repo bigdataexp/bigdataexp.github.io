38d254113bf12bdc6c5c9a05eca422a2dd8528fd
Shilpa Jain <shilpa.jain2014@outlook.com>
2019-04-26 21:45:21 -0400

    adding files

--- src/components/Root.jsx
+++
@@ -1,7 +0,0 @@
-'use strict';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Dashboard from 'components/Dashboard';
-
-// Render the Dashboard component into the element with ID 'react-root'
-ReactDOM.render((<Dashboard />), document.getElementById('react-root'));
