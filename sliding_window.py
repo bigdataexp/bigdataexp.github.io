23b87e116e3fea3747b60b9a5051df0029f1d891
bigdataexp <50034655+bigdataexp@users.noreply.github.com>
2022-05-25 11:38:02 -0700

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -1,1 +1,14 @@
+# Sliding Window Pattern has following question variations:
+# Fixed Length of window and iterable data structure and ask is to find something like max su, min, contains etc.
+# Dynamic Length of Window(it can grow and shrink) and from iterable data structure, find a subset with something like sum, max sum, avg, contains, shortest.
+# Dynamic Length of Window with a Auxillary data structure.
+
+
+
+
+
+
+
+
+
 
