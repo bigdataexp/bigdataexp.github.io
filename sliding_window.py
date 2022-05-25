e381b86aba81397ffc9fe7bd2cb92731d3fa3ddb
bigdataexp <50034655+bigdataexp@users.noreply.github.com>
2022-05-25 11:39:03 -0700

    Update sliding_window.py

--- sliding_window.py
+++ sliding_window.py
@@ -4,6 +4,27 @@
 # Dynamic Length of Window with a Auxillary data structure.
 
 
+#First problem with a fixed length of window, find the maxinum sum of continous subarray.
+
+#Given an array of positive numbers and a positive number ‘k’, find the maximum sum of any contiguous subarray of size ‘k’.
+def max_subarray_with_fixed_size(arr, k):
+    
+    current_sum = 0
+    max_sum = 0
+
+    for i, value in enumerate(arr):
+        current_sum += value
+        
+        if i >= (k-1):
+            max_sum = max(current_sum, max_sum)
+            current_sum -= arr[i-(k-1)]
+            
+    print(max_sum)
+    
+    
+    
+max_subarray_with_fixed_size([2, 3, 4, 1, 5], 2)
+    
 
 
 
